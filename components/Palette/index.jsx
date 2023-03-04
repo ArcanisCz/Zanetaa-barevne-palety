15c9d15e1a4deb268dbef2461d22a9ba0201fdc5
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-03-04 20:12:23 +0100

    pokus palettes

--- components/Palette/index.jsx
+++ components/Palette/index.jsx
@@ -3,30 +3,26 @@ import { palettes } from '../../src/palettes';
 import { palettes } from './palettes.js';
 import './style.css';
 
-export const paletteData = ({ name, image, atribution }) => {
+export const paletteData = ({
+  name,
+  image,
+  attribution,
+  description,
+  direction,
+}) => {
   return (
     <div className="palette">
       <div className="palette-scheme palette-scheme--vertical">
-        <img
-          className="scheme-image"
-          src="/img/mimosa-retreat.jpg"
-          alt="Mimosa Retreat"
-        />
+        <img className="scheme-image" src={image} alt={name} />
       </div>
       <div className="palette-info">
-        <h2>Mimose Retreat</h2>
-        <p>
-          Brown, combined with shades of yellow, is a rather unusual combination
-          that can be called really sweet. It is both citrusy, caramel, and has
-          a slight hint of cinnamon, which gives it a special versatility. In
-          summer it will invigorate, make you act and move, and in winter it
-          will warm and relax.
-        </p>
+        <h2>{name}</h2>
+        <p>{description}</p>
 
         <p>
           Photo by{' '}
-          <a href="https://unsplash.com/photos/XkiONXX7i4o" target="_blank">
-            Sara Cervera
+          <a href={attribution.url} target="_blank">
+            {attribution.name}
           </a>
           .
         </p>
