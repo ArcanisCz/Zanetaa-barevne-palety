2adce36655d4f29f3b0388cfe4d128af3c1a43a0
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-03-05 12:21:22 +0100

    podbarvení - ale jen v jedné řadě

--- components/SchemeColor/index.jsx
+++ components/SchemeColor/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 
 export const SchemeColor = ({ colors }) => {
   return (
-    <div className="scheme_color" style={{ backgroundColor: colors }}>
+    <div className="scheme_color" style={{ backgroundColor: colors[0] }}>
       {colors}
     </div>
   );
