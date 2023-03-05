d2b54bbe91a3cebfc116205db8f35c474640eb74
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-03-05 12:08:39 +0100

    pokusy o opravu

--- components/SchemeColor/index.jsx
+++ components/SchemeColor/index.jsx
@@ -3,8 +3,9 @@ import './style.css';
 
 export const SchemeColor = ({ colors }) => {
   return (
-    <div className="scheme_color" style={{ backgroundColor: color }}>
+    <div className="scheme_color" style={{ backgroundColor: color[0] }}>
       {colors}
     </div>
   );
 };
+ß;
